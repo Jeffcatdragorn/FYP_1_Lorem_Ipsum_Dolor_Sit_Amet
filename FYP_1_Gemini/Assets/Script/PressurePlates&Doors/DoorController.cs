821a58438a490d82b070a414499beda8cb8cb254
Jane0705 <janeli0705@gmail.com>
2022-05-11 04:10:48 +0800

    Pressure Plate & Door (with OpenOnceOnly door type)

---
+++ FYP_1_Gemini/Assets/Script/PressurePlates&Doors/DoorController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Animator door = null;
+    [SerializeField] private Animator pressurePlate = null;
+
+    [Header("Door type")] //only tick one door type in the inspector
+    [SerializeField] private bool openOnceOnlyDoor = false;
+
+    [Header("Pressure Plate Type")] //only tick one pressure plate type in the inspector
+    [SerializeField] private bool openDoorTrigger = false;
+    //[SerializeField] private bool closeDoorTrigger = false;
+
+    [Header ("Animation names")] //just in case got different animations for doors and pressure plates
+    [SerializeField] private string doorOpen = "DoorOpening";
+    [SerializeField] private string doorClose = "DoorClosing";
+    [SerializeField] private string pressurePlatePressed = "PressurePlatePressed";
+    [SerializeField] private string pressurePlateReleased = "PressurePlateReleased";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")) //change the tag to the object that is going to be placed on the pressure plate
+        {
+            if (openDoorTrigger == true)
+            {
+                door.Play(doorOpen, 0, 0.0f);
+                pressurePlate.Play(pressurePlatePressed, 0, 0.0f);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) //change the tag to the object that is going to be placed on the pressure plate
+        {
+            //closeDoorTrigger = true;
+            if(openOnceOnlyDoor == false)
+            {
+                door.Play(doorClose, 0, 0.0f);
+                pressurePlate.Play(pressurePlateReleased, 0, 0.0f);
+            }
+        }
+    }
+}
