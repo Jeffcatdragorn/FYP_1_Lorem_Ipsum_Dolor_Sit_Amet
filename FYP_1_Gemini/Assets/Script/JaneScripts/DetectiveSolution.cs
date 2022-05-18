1f2cc522c43cbdb69d2096a544a6331ba7a81e51
Jane0705 <janeli0705@gmail.com>
2022-05-18 19:20:28 +0800

    Spam F

--- FYP_1_Gemini/Assets/Script/JaneScripts/DetectiveSolution.cs
+++ FYP_1_Gemini/Assets/Script/JaneScripts/DetectiveSolution.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class DetectiveSolution : MonoBehaviour
 {
     InteractWithObjects input;
+    public GameObject door;
     private int inputCount = 0;
     private bool inputChecker;
+    public Animator doorAnimator;
 
     private void Awake()
     {
@@ -35,14 +37,22 @@ public class DetectiveSolution : MonoBehaviour
 
     public void OpenDoorWithShooting() //grappling door & pulling it open
     {
-        inputCount++;
+        //currentDoorPos = door.transform.position;
+        doorAnimator.enabled = false;
 
-        if (inputCount == 5)
+        if (inputCount == 7) //if press 7 times already then stop opening the door
         {
-           
+            inputCount = 7;
+            door.transform.position = new Vector3(0.0f, 0.0f, 7.0f);
+            //Destroy(door);
         }
-
-       
+        else
+        {
+            inputCount++;
+            door.transform.position = new Vector3(door.transform.position.x, door.transform.position.y, door.transform.position.z + 1.0f);
+            Debug.Log("door.transform.position = " + door.transform.position);
+        }
+        
         Debug.Log("OpenDoorWithShooting ");
     }
 }
