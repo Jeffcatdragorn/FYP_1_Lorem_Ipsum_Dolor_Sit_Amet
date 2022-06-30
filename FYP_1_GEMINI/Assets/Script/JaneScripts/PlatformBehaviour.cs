3765be901bae1cc4a439183198d37eff1c04f8b8
Jane0705 <85795728+Jane0705@users.noreply.github.com>
2022-06-30 16:44:01 +0800

    Breakable Tile Behavior + Tile snapping only when generators are off

--- FYP_1_GEMINI/Assets/Script/JaneScripts/PlatformBehaviour.cs
+++ FYP_1_GEMINI/Assets/Script/JaneScripts/PlatformBehaviour.cs
@@ -13,18 +13,20 @@ public class PlatformBehaviour : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if(other.GetComponent<TileBehaviour>().type == this.type && connectedTile == null)
+        if(other.GetComponent<TileBehaviour>().type == this.type) //tile can only be snapped when generators are off
         {
-            pickUpObjects.LetGoOfObject();
-            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
-            other.gameObject.transform.position = level1.transform.position;
-            other.gameObject.transform.rotation = level1.transform.rotation;
-            other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
-            other.gameObject.layer = 16 ; //layer = tiles
-            //other.gameObject.transform.parent = gameObject.transform;
-
-            connectedTile = other.gameObject;
+            if (connectedTile == null && generator1State == false && generator2State == false)
+            {
+                pickUpObjects.LetGoOfObject();
+                other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                other.gameObject.transform.position = level1.transform.position;
+                other.gameObject.transform.rotation = level1.transform.rotation;
+                other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+                other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+                other.gameObject.layer = 16; //layer = tiles
+                other.gameObject.GetComponent<TileBehaviour>().isConnectedTile = true;
+                connectedTile = other.gameObject;
+            }
         }
     }
 
@@ -53,27 +55,36 @@ public class PlatformBehaviour : MonoBehaviour
             connectedTile.transform.position = level1.transform.position;
             connectedTile.transform.rotation = level1.transform.rotation;
         }
-
-        Debug.Log("generator1State = " + generator1State);
-        Debug.Log("generator2State = " + generator2State);
     }
 
     public void GeneratorInteract(string generatorTag)
     {
         if(generatorTag == "gen1")
         {
-            if(generator1State == true)
+            if(generator1State == true) //turn off generator 1
+            {
                 generator1State = false;
-            else
+                TileBehaviour.startBreakable = false;
+            }
+            else //turn on generator 1
+            {
                 generator1State = true;
+                TileBehaviour.startBreakable = true;
+            }
         }
 
         else if(generatorTag == "gen2")
         {
-            if (generator2State == true)
+            if (generator2State == true) //turn off generator 2
+            {
                 generator2State = false;
-            else
+                TileBehaviour.startBreakable = false;
+            }
+            else //turn on generator 2
+            {
                 generator2State = true;
+                TileBehaviour.startBreakable = true;
+            }
         }
     }
 }
--- FYP_1_GEMINI/Assets/Script/JaneScripts/TileBehaviour.cs
+++ FYP_1_GEMINI/Assets/Script/JaneScripts/TileBehaviour.cs
@@ -5,4 +5,42 @@ using UnityEngine;
 public class TileBehaviour : MonoBehaviour
 {
     public int type;
+    public bool isConnectedTile;
+    public bool playerTouched = false;
+    public static bool startBreakable = false;
+    [SerializeField] float breakingTimeCounter;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (type == 2 && isConnectedTile == true && startBreakable == true)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                playerTouched = true;
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        if (playerTouched == true)
+         SetBreakingTime();
+
+        if(breakingTimeCounter <= 0)
+            Destroy(gameObject);
+    }
+
+    void SetBreakingTime()
+    {
+        if (breakingTimeCounter > 0)
+        {
+            breakingTimeCounter -= Time.deltaTime;
+        }
+
+        if (breakingTimeCounter <= 0)
+        {
+            breakingTimeCounter = 0.0f;
+        }
+    }
 }
