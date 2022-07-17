b3a47d534235fcf72254304db40a4376feae9510
0128337AmsyarKhan <90510902+0128337AmsyarKhan@users.noreply.github.com>
2022-07-17 10:32:43 +0800

    Refine lights
    Only in prison area.

---
+++ FYP_1_Gemini/Assets/SirenLights.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SirenLights : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,100,0) * Time.deltaTime);
+    }
+}
