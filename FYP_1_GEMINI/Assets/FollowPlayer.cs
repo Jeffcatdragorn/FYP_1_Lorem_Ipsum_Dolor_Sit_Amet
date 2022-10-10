e932a428f50f548cb46417ce4e6acdc2ef4b8577
Jeffcatdragorn <cat.dragron@gmail.com>
2022-10-10 23:24:57 +0800

    updated phaser bugs

--- FYP_1_GEMINI/Assets/FollowPlayer.cs
+++ FYP_1_GEMINI/Assets/FollowPlayer.cs
@@ -8,16 +8,25 @@ public class FollowPlayer : MonoBehaviour
     public float speed = 1f;
     private float distance;
     private float randY;
+    private Phaser2_Manager phsr;
+    private float time;
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         randY = Random.Range(2, 13);
+        phsr = GameObject.FindGameObjectWithTag("Phaser").GetComponent<Phaser2_Manager>();
     }
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, player.position, speed*Time.deltaTime);
-        transform.position = new Vector3(transform.position.x, randY , transform.position.z);
-        //distance = Vector3.Distance(transform.position, player.position);
+        if (phsr.AliveP1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, speed*Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, randY , transform.position.z);
+            //distance = Vector3.Distance(transform.position, player.position);
+        }
+        time += Time.deltaTime;
+        if (time > 5) Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
