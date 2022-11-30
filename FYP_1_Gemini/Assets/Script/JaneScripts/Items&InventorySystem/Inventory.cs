4cebbe31d12d894136e3e93ba6be33a710060adf
Jane0705 <janeli0705@gmail.com>
2022-12-01 03:18:36 +0800

    Inventory Stackable Items Fixes

--- FYP_1_Gemini/Assets/Script/JaneScripts/Items&InventorySystem/Inventory.cs
+++ FYP_1_Gemini/Assets/Script/JaneScripts/Items&InventorySystem/Inventory.cs
@@ -150,7 +150,7 @@ public class Inventory : MonoBehaviour
             {
                 if (items[i].name == item.name)
                 {
-                    items[i].itemAmount += item.itemAmount; 
+                    items[i].itemAmount++; 
 
                     if (onItemChangedCallback != null)
                     {
